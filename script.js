1e9c3d39d2892ca9572c34661d0ed1a27a2e3818
BrenML <brendita_mtz@live.com.mx>
2020-06-16 12:39:37 -0500

    modify scale

--- script.js
+++ script.js
@@ -7,21 +7,21 @@ window.onload = () => {
 var models = [
     {
         url: './assets/magnemite/scene.gltf',
-        scale: '0.5 0.5 0.5',
+        scale: '0.05 0.05 0.05',
         info: 'Magnemite, Lv. 5, HP 10/10',
         rotation: '0 180 0',
         position: '0.15 -0.5 3'
     },
     {
         url: './assets/charmander/model.gltf',
-        scale: '0.2 0.2 0.2',
+        scale: '2 2 2',
         rotation: '0 180 0',
         info: 'Charmander',
         position: '0.15 -0.5 3'
     },
     {
         url: './assets/dragonite/scene.gltf',
-        scale: '0.08 0.08 0.08',
+        scale: '0.02 0.02 0.02',
         rotation: '0 180 0',
         info: 'Dragonite, Lv. 99, HP 150/150',
         position: '0.15 -0.5 3'
